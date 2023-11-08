1858caa0bb444499651ee3305b9d3fe755c829f4
Samot Point <samotpoint@gmail.com>
2023-11-08 01:37:16 -0500

    Add Dockerfile

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -15,11 +15,12 @@ printSubTitle "$CUDA_ARCH_SM"
 read -p "Continue (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   scripts/stop.sh
-  git reset --hard
   printSubTitle "Deleting XENGPUMiner"
   rm -rf XENGPUMiner
   printSubTitle "Updating XENGPUMiner-monitoring"
+  git stash
   git pull origin main
+  git stash pop
   sudo chmod -R 700 scripts
   printSubTitle "Installing and starting miner/monitoring applications"
   scripts/install.sh && scripts/start.sh && scripts/monitor.sh

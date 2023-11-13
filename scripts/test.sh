6b07a7a7e885c99d971b9c98eb42dea6e1e01007
Samot Point <samotpoint@gmail.com>
2023-11-13 12:56:24 -0500

    update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,13 @@
 source scripts/utils.sh
 cd_xengpuminer
 
+MINER_COUNT=$(ps -x -o command | grep -c "python3 miner.py")
+XEN_GPU_MINER_COUNT=$(ps -x -o command | grep -c "xengpuminer")
+
+printTitle "Validate miner.py and xengpuminer process count"
+printSubTitle "miner.py ($MINER_COUNT), should be greater then 1"
+printSubTitle "xengpuminer ($XEN_GPU_MINER_COUNT), should be GPU count + 1"
+
 printTitle "Running watcher locally"
 
 python3 watch.py --local=true || printSubTitle "Error something went wrong!"
